9eb7e66636da85be3774be4ff741ca6f9bf92597
Renze Post <renze.post@gmail.com>
2026-01-22 22:32:34 +0100

    Fix brackets and indents

--- queries/brackets.scm
+++ queries/brackets.scm
@@ -1,8 +1,6 @@
-("(" @open ")" @close)
-("{" @open "}" @close)
-("[" @open "]" @close)
+; Dataform interpolation brackets
+; Note: Parentheses, braces, and square brackets are anonymous tokens
+; in this grammar and cannot be directly matched in queries.
+; Only named nodes like interpolation can have their delimiters captured.
 
-; Dataform interpolation
-(interpolation
-  "${" @open
-  "}" @close)
+(interpolation) @container
--- queries/indents.scm
+++ queries/indents.scm
@@ -1,26 +1,17 @@
 ; Indentation rules for Dataform SQLX files
+; Note: Braces are anonymous tokens, so we match parent nodes instead
 
 ; Indent inside config blocks
-(config_block
-  "{" @indent
-  "}" @dedent)
+(config_block) @indent
 
 ; Indent inside js blocks
-(js_block
-  "{" @indent
-  "}" @dedent)
+(js_block) @indent
 
 ; Indent inside pre_operations blocks
-(pre_operations_block
-  "{" @indent
-  "}" @dedent)
+(pre_operations_block) @indent
 
 ; Indent inside post_operations blocks
-(post_operations_block
-  "{" @indent
-  "}" @dedent)
+(post_operations_block) @indent
 
 ; Indent inside interpolations
-(interpolation
-  "${" @indent
-  "}" @dedent)
+(interpolation) @indent
